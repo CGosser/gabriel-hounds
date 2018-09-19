f5e2bbc548428657fe171bb6f9310719713777a2
Chris Gosser and Evan Filkins <evan@>
2018-09-19 16:53:14 -0700

    arrange elements in grid, add hover to elements

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import './css/styles.css';
 import './sass/styles.scss';
 
 console.log("heyyy there!");
+
+$(document).ready(function() {
+  console.log("Jquery is running");
+  $("img").hover(function() {
+    console.log("So there's hovering.");
+    $(".bg-swap").toggle();
+  })
+
+})
